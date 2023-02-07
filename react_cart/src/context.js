775410b49dfb7e387ea884ebc3ccf1b3c6dea4af
TarasKing <ivanovtaras@live.ru>
2023-02-06 22:04:53 -0800

    finish

--- react_cart/src/context.js
+++ react_cart/src/context.js
@@ -1,56 +1,55 @@
-import React, { useState, useContext, useReducer, useEffect } from 'react'
-import cartItems from './data'
-import reducer from './reducer'
+import React, { useContext, useReducer, useEffect } from "react";
+import cartItems from "./data";
+import reducer from "./reducer";
 
-const url = 'https://course-api.com/react-useReducer-cart-project'
+const url = "https://course-api.com/react-useReducer-cart-project";
 
-const AppContext = React.createContext()
+const AppContext = React.createContext();
 
 const initialState = {
   loading: false,
   cart: cartItems,
   total: 0,
   amount: 0,
-}
+};
 
 const AppProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer, initialState)
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  const clearCart = () => { 
-    dispatch({ type: 'CLEAR_CART' })
-  }
+  const clearCart = () => {
+    dispatch({ type: "CLEAR_CART" });
+  };
   const remove = (id) => {
-    dispatch({ type: 'REMOVE', payload: id })
-  }
+    dispatch({ type: "REMOVE", payload: id });
+  };
   const increase = (id) => {
-    dispatch({ type: 'INCREASE', payload: id })
-  }
+    dispatch({ type: "INCREASE", payload: id });
+  };
   const decrease = (id) => {
-    dispatch({ type: 'DECREASE', payload: id })
-  }
+    dispatch({ type: "DECREASE", payload: id });
+  };
 
-  const fetchData = async () => { 
-    dispatch({ type: 'LOADING' })
+  const fetchData = async () => {
+    dispatch({ type: "LOADING" });
 
-    const response = await fetch(url)
-    
-    const cart = await response.json()
-    dispatch({ type: 'DISPLAY_ITEMS', payload: cart })
-  }
- 
-  useEffect(() => { 
-    fetchData()
-  }, [])
-  
-  const toggleAmount = (id, type) => { 
-    dispatch({ type: 'TOGGLE_AMOUNT', payload: { id, type } })
-  }
+    const response = await fetch(url);
 
-  useEffect(() => { 
+    const cart = await response.json();
+    dispatch({ type: "DISPLAY_ITEMS", payload: cart });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const toggleAmount = (id, type) => {
+    dispatch({ type: "TOGGLE_AMOUNT", payload: { id, type } });
+  };
+
+  useEffect(() => {
     // console.log('changed')
-    dispatch({ type: 'GET_TOTALS' })
-  }, [state.cart])
-
+    dispatch({ type: "GET_TOTALS" });
+  }, [state.cart]);
 
   return (
     <AppContext.Provider
@@ -65,11 +64,11 @@ const AppProvider = ({ children }) => {
     >
       {children}
     </AppContext.Provider>
-  )
-}
+  );
+};
 // make sure use
 export const useGlobalContext = () => {
-  return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
 
-export { AppContext, AppProvider }
+export { AppContext, AppProvider };
--- react_cart/src/data.js
+++ react_cart/src/data.js
@@ -1,26 +1,23 @@
 export default [
   {
     id: 1,
-    title: 'Samsung Galaxy S7',
+    title: "Samsung Galaxy S7",
     price: 599.99,
-    img:
-      'https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-s7--.jpg',
+    img: "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-s7--.jpg",
     amount: 1,
   },
   {
     id: 2,
-    title: 'google pixel ',
+    title: "google pixel ",
     price: 499.99,
-    img:
-      'https://lh3.googleusercontent.com/5wIYsY-0T7ujv4PytJ-gVZOKkoVA8j0t0ldq-SsuJAbHq3w3X6udwIgyE0wpwyRUXTxHp_wkd6wz76bMlv-976dWLAEfrZLTKQ=rw-e365-w240',
+    img: "https://lh3.googleusercontent.com/5wIYsY-0T7ujv4PytJ-gVZOKkoVA8j0t0ldq-SsuJAbHq3w3X6udwIgyE0wpwyRUXTxHp_wkd6wz76bMlv-976dWLAEfrZLTKQ=rw-e365-w240",
     amount: 1,
   },
   {
     id: 3,
-    title: 'Xiaomi Redmi Note 2',
+    title: "Xiaomi Redmi Note 2",
     price: 699.99,
-    img:
-      'https://fdn2.gsmarena.com/vv/bigpic/xiaomi-redmi-note-2--.jpg',
+    img: "https://fdn2.gsmarena.com/vv/bigpic/xiaomi-redmi-note-2--.jpg",
     amount: 1,
   },
 ];
